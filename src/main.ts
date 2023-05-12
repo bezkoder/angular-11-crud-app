2cbad0404c238ae5ff5f2c02be0ce58917e0bf3c
Rajender Polu <rajender.polu@autorabit.com>
2023-05-12 16:53:46 +0530

    Revert "Update main.ts"
    This reverts commit 9b8812e5dac0ca994d51e00fc284eea1673811a0.

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
