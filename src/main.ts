9b8812e5dac0ca994d51e00fc284eea1673811a0
Rajender Polu <rajender.polu@autorabit.com>
2023-05-12 16:52:31 +0530

    Update main.ts
    Updated

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,5 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
